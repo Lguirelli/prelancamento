80223c764cef0311015c9ad06e416c91f3dcaa83
Lguirelli <68300894+Lguirelli@users.noreply.github.com>
2025-08-28 00:25:16 -0300

    Delete footer.js

--- footer.js
+++
@@ -1,6 +0,0 @@
-fetch('footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('site-footer').innerHTML = data;
-  })
-  .catch(error => console.error('Erro ao carregar o footer:', error));
